3de062c446c427a5e67d24ee22004efa909b838a
root <root@srv1129427.hstgr.cloud>
2025-12-17 11:18:55 +0000

    Mise à jour de ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  apps : [{
-    name   : "gestion-commerciale",
-    script : ".next/standalone/server.js", // <-- CHANGEMENT ICI
-    args : "",                             // <-- CHANGEMENT ICI
-    cwd: "/home/ghost/gestion-commerciale",
-    env: {
-      NODE_ENV: "production",
-      PORT: "4000",
-      // VEUILLEZ VERIFIER CES VALEURS
-      DATABASE_URL: "mysql://ghostuser:password123!@localhost:3306/gestion_commerciale",
-      NEXTAUTH_SECRET: "FrziolYfWSVRH501kkojbHszR/Cts7KA3wMzIMZklyY=",
-      // REMPLACER PAR VOTRE VRAIE URL PUBLIQUE
-      NEXTAUTH_URL: "http://72.61.109.17:4000" // Utilisez l'IP et le port 3002
-    }
-  }]
+  apps: [
+    {
+      name: "gestion-commerciale",
+      script: "npx",               // Utiliser npx pour lancer next
+      args: "next start -p 4000",  // Port de l'application
+      cwd: "/var/www/gestion-commerciale",
+      env: {
+        NODE_ENV: "production",
+        PORT: "4000",
+        DATABASE_URL: "mysql://ghostuser:Passw0rdG%40st%21on@localhost:3306/gestion_commerciale",
+        NEXTAUTH_SECRET: "f9554aed97a0e2bf80f16a2efcac086b99a2591a2df052b76d78447f4ad37863",
+        NEXTAUTH_URL: "http://72.61.109.17:4000"
+      }
+    }
+  ]
 };
+
